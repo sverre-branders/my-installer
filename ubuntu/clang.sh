95ebd2a7a41ba06f8fb2433a1f233215f5707bb0
sverre-branders <sverre.branders@protonmail.com>
2025-08-08 10:14:28 +0200

    Added clang install

---
+++ ubuntu/clang.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+DRY_RUN=true
+
+install_clang ()
+{
+    if $DRY_RUN; then
+        echo -e "DRY RUN:\tsudo apt update"
+    else
+        sudo apt update
+    fi
+
+    local packages=("clang" "clang-tidy")
+    for package in "${packages[@]}"; do
+        apt-cache show "$package" > /dev/null 2>&1 || {
+            echo "Package '$package' not available!"
+            exit 1
+        }
+    done
+
+    for package in "${packages[@]}"; do
+        if $DRY_RUN; then
+            echo -e "DRY RUN:\tsudo apt install -y $package"
+        else
+            sudo apt install -y "$package"
+        fi
+    done
+}
+
+install_clang
+
